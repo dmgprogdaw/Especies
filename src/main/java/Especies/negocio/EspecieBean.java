de8ee7aae34fa6f982c941a45695a723a63f6ef6
David <zukoclase96@gmail.com>
2019-11-12 08:43:43 +0100

    Eliminados imports innecesarios

--- src/main/java/Especies/negocio/EspecieBean.java
+++ src/main/java/Especies/negocio/EspecieBean.java
@@ -3,8 +3,6 @@ package Especies.negocio;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
--- src/main/java/Especies/negocio/InformeBean.java
+++ src/main/java/Especies/negocio/InformeBean.java
@@ -41,7 +41,6 @@ public class InformeBean {
 	}
 
 	
-	
 	public List<BiologoBean> getBiologosInforme() {
 		return biologosInforme;
 	}
